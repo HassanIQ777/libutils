5de2593f24409fb5dc91732b32b90c0aa2e51ed7
HassanIQ777 <hassaniq793@gmail.com>
2026-04-18 00:36:07 +0300

    CLIParser objects can now be initialized without any contructor arguments, later they can be set with CLIParser::setArgs

--- src/CLIParser.hpp
+++ src/CLIParser.hpp
@@ -8,31 +8,31 @@ Last update: 2025-Sep-20 */
 #include <string>
 
 class CLIParser {
-public:
-  CLIParser(int argc, char *argvec[]) : m_argc(argc), m_argv(argvec) {}
-
-  // FUNCTION NAMES NEED A FUCKING REVISION
-  bool hasFlag(const std::string &flag)
-      const; // true if a certain flag(argument) exists in argv
-
-  int getIndex(const std::string &arg)
-      const; // returns the index of an argument in argv (-1 if argument doesn't
-             // exist), use it to find arguments
-
-  std::string getArg(const int &index) const; // returns the argument at index
-
-  std::string getValue(const std::string &arg)
-      const; // returns value after flag, for example you can find filename
-             // after --file FILENAME like: getValue("--file")
-
-  int getArgc() const;
-
-  char **getArgv() const;
-
-  // NOTE : some function names might need revision
 private:
   int m_argc;
   char **m_argv; // i love-hate pointers
+
+public:
+  CLIParser(int argc, char *argvec[]) : m_argc(argc), m_argv(argvec) {}
+  CLIParser() {}
+
+  // true if a certain flag(argument) exists in argv
+  bool hasFlag(const std::string &flag) const;
+
+  // returns the index of an argument in argv (-1 if argument doesn't
+  // exist), use it to find arguments
+  int getIndex(const std::string &arg) const;
+
+  // returns the argument at index
+  std::string getArg(const int &index) const;
+
+  // returns value after flag, for example you can find filename
+  // after --file FILENAME like: getValue("--file")
+  std::string getValue(const std::string &arg) const;
+
+  int getArgc() const;
+  char **getArgv() const;
+  void setArgs(int argc, char *argvec[]);
 };
 
 inline bool CLIParser::hasFlag(
@@ -70,5 +70,10 @@ inline std::string CLIParser::getValue(const std::string &arg) const {
 inline int CLIParser::getArgc() const { return m_argc; }
 
 inline char **CLIParser::getArgv() const { return m_argv; }
+
+inline void CLIParser::setArgs(int argc, char *argvec[]) {
+  m_argc = argc;
+  m_argv = argvec;
+}
 
 #endif // CLIParser.hpp
