fab8efb67812c65526cd76bc2482a838205d1513
HassanIQ777 <hassaniq793@gmail.com>
2026-06-15 19:02:49 +0300

    A new class for multi-threaded loading bars that are automatically managed with RAII

---
+++ src/LoadingBar.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <vector>
+
+// you can use these as loading bars:
+// {"⠋", "⠙", "⠹", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"};
+// {"🕐","🕑","🕒","🕓","🕔","🕕","🕖","🕗","🕘","🕙","🕚","🕛"}
+// {"|", "/", "-", "\\"}
+// {".  ", ".. ", "..."}
+// {"▏","▎","▍","▌","▋","▊","▉"}
+
+namespace Loadingbar {
+
+class Spinner {
+  std::atomic<bool> done;
+  std::thread thread;
+
+public:
+  Spinner(const std::vector<std::string> &loading_bar, int sleep_duration,
+          const std::string &msg = "Loading")
+      : done(false),
+        thread([this, loading_bar, sleep_duration, msg]() { // capture by value
+          size_t i = 0;
+          while (!done.load(std::memory_order_acquire)) {
+            std::cout << "\r" << loading_bar[i % loading_bar.size()] << " "
+                      << msg
+                      << "         " // trailing spaces to clear leftover chars
+                      << std::flush;
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(sleep_duration));
+            i++;
+          }
+          std::cout << "\r✓ " << msg << " done!          \n" << std::flush;
+        }) {}
+
+  void stop() {
+    done = true;
+    if (thread.joinable())
+      thread.join();
+  }
+
+  ~Spinner() { stop(); }
+
+  Spinner(const Spinner &) = delete;
+  Spinner &operator=(const Spinner &) = delete;
+};
+
+} // namespace Loadingbar
