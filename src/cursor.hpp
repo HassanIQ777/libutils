6645069219ee905eaecd89e8be55a33786cef6a0
HassanIQ777 <hassaniq793@gmail.com>
2026-06-16 23:57:47 +0300

    Added cursor.hpp

---
+++ src/cursor.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+namespace cursor {
+inline void up(int n = 1) { std::cout << "\033[" << n << "A"; }
+inline void down(int n = 1) { std::cout << "\033[" << n << "B"; }
+inline void right(int n = 1) { std::cout << "\033[" << n << "C"; }
+inline void left(int n = 1) { std::cout << "\033[" << n << "D"; }
+
+inline void home() { std::cout << "\033[H"; }
+inline void moveTo(int row, int col) {
+  std::cout << "\033[" << row << ";" << col << "H";
+}
+
+inline void clearLine() { std::cout << "\033[2K"; }
+inline void clearToEnd() { std::cout << "\033[K"; }
+
+inline void save() { std::cout << "\033[s"; }
+inline void restore() { std::cout << "\033[u"; }
+
+inline void hide() { std::cout << "\033[?25l"; }
+inline void show() { std::cout << "\033[?25h"; }
+
+// flush is separate so you can batch moves without flushing each one
+inline void flush() { std::cout.flush(); }
+} // namespace cursor
